f6a430c59856158927498532c3811275faad49bb
maxrjj <maxrjj@hotmail.com>
2017-04-19 17:55:27 -0300

    Commit Interpolação Basica

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = "http://www.mvasconcelos.com";
+  constructor() { }
+  Booleana = true;
+getValor(){
+  return 1;
+}
+retornaBool(){
+return true;
+}
+
+  ngOnInit() {
+  }
+
+}
